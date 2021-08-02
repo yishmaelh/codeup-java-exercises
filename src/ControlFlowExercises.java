82f6fab80cf0fffa551a51b6daaef84d18503fd6
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-02 16:50:27 -0500

    using for loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,14 +2,14 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int startingNumber = 5;
-        while (startingNumber <= 15) {
-            System.out.println("Current number is: " + startingNumber);
-            startingNumber++;
-
-
-        }
-
+//        int startingNumber = 5;
+//        while (startingNumber <= 15) {
+//            System.out.println("Current number is: " + startingNumber);
+//            startingNumber++;
+//
+//
+//        }
+//
 //        int addTwos = 0;
 //        do {
 //            System.out.println(addTwos);
@@ -21,12 +21,18 @@ public class ControlFlowExercises {
 //            System.out.println(minusFive);
 //            minusFive -= 5;
 //        } while (minusFive >= -10);
+//
+//        int square = 2;
+//        do {
+//            System.out.println(square);
+//            square *= square;
+//        } while (square < 1000000 & (square > 1));
 
-        int square = 2;
-        do {
-            System.out.println(square);
-            square *= square;
-        } while (square < 1000000 & (square > 1));
+        // Refactoring previous code via for loop
+
+        for (int startNumber = 5; startNumber <= 15; startNumber++) {
+            System.out.print("\n" + startNumber);
+        }
 
     }
 }
