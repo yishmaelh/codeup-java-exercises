dbb053a7ccdbda1aeeda73ba2b353a279f7ae856
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-02 16:10:03 -0500

    step one done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,13 @@ public class ControlFlowExercises {
 
 public static void main(String[] args) {
 
+int startingNumber = 5;
+while  (startingNumber <= 15){
+    System.out.println("Your number is: " + startingNumber);
+    startingNumber++;
 
+
+}
 
 }
 }
