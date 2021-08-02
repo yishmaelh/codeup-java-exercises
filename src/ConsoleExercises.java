9763e3f7d2a743c8fa85f20ed1c385a8d3664ea2
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-02 10:11:28 -0500

    pi

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,10 @@
+
+
 public class ConsoleExercises {
 
     public static void main (String[] args) {
-
+        double pi = 3.14159;
+        String piEstimate = "The value of pi is approximately: ";
+        System.out.printf("%s %.2f", piEstimate, pi);
     }
 }
