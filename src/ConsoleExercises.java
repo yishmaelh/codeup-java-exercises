fa337dea6322304d10c243375b7cb4fc915959e4
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-02 10:32:31 -0500

    accepts and returns user number

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class ConsoleExercises {
 
@@ -6,5 +6,13 @@ public class ConsoleExercises {
         double pi = 3.14159;
         String piEstimate = "The value of pi is approximately: ";
         System.out.printf("%s %.2f", piEstimate, pi);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(  "\n" +"Enter a number: ");
+        int userInt = scanner.nextInt();
+        System.out.print("You entered: " + "" + userInt + "\n");
+
     }
+
+
 }
