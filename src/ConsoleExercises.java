0d0b0b08016b152a766ab6abfbb159e91874b69d
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-02 16:26:05 -0500

    addTwo do loop

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,16 +2,15 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         double pi = 3.14159;
         String piEstimate = "The value of pi is approximately: ";
         System.out.printf("%s %.2f", piEstimate, pi);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print(  "\n" +"Enter a number: ");
+        System.out.print("\n" + "Enter a number: ");
         int userInt = scanner.nextInt();
         System.out.print("You entered: " + "" + userInt + "\n");
-
 
 
         System.out.print("Enter three different words:");
--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,14 +1,20 @@
 public class ControlFlowExercises {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-int startingNumber = 5;
-while  (startingNumber <= 15){
-    System.out.println("Your number is: " + startingNumber);
-    startingNumber++;
+        int startingNumber = 5;
+        while (startingNumber <= 15) {
+            System.out.println("Current number is: " + startingNumber);
+            startingNumber++;
 
 
-}
+        }
 
-}
+        int addTwos = 0;
+        do {
+            System.out.println(addTwos);
+            addTwos += 2;
+        } while (addTwos <= 100);
+
+    }
 }
