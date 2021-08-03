182c4b41f78c5fe189b646d993adb716bb4e9bc0
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-03 07:58:04 -0500

    for loops cont

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,37 +2,49 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-//        int startingNumber = 5;
-//        while (startingNumber <= 15) {
-//            System.out.println("Current number is: " + startingNumber);
-//            startingNumber++;
-//
-//
-//        }
-//
-//        int addTwos = 0;
-//        do {
-//            System.out.println(addTwos);
-//            addTwos += 2;
-//        } while (addTwos <= 100);
-//
-//        int minusFive = 100;
-//        do {
-//            System.out.println(minusFive);
-//            minusFive -= 5;
-//        } while (minusFive >= -10);
-//
-//        int square = 2;
-//        do {
-//            System.out.println(square);
-//            square *= square;
-//        } while (square < 1000000 & (square > 1));
+        int startingNumber = 5;
+        while (startingNumber <= 15) {
+            System.out.println("Current number is: " + startingNumber);
+            startingNumber++;
+
+
+        }
+
+        int addTwos = 0;
+        do {
+            System.out.println(addTwos);
+            addTwos += 2;
+        } while (addTwos <= 100);
+
+        int minusFive = 100;
+        do {
+            System.out.println(minusFive);
+            minusFive -= 5;
+        } while (minusFive >= -10);
+
+        int square = 2;
+        do {
+            System.out.println(square);
+            square *= square;
+        } while (square < 1000000 & (square > 1));
 
         // Refactoring previous code via for loop
 
-        for (int startNumber = 5; startNumber <= 15; startNumber++) {
-            System.out.print("\n" + startNumber);
-        }
+//        for (int startNumber = 5; startNumber <= 15; startNumber++) {
+//            System.out.print("\n" + startNumber);
+//        }
+//
+//        for (int plusTwo = 2; plusTwo <= 100; plusTwo += 2){
+//            System.out.print("\n" + plusTwo);
+//        }
+//
+//        for (int squared = 2; squared < 1000000 & (squared > 1); squared *= squared) {
+//            System.out.print("\n" + squared);
+//        }
+//
+//        for (int lessFive = 100; lessFive >= -10; lessFive -= 5) {
+//            System.out.print("\n" + lessFive);
+//        }
 
     }
 }
