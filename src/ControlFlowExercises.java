60cec5681c6083ed5f0141a7040b7ec7e84193ef
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-03 08:33:17 -0500

    fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,31 +2,31 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int startingNumber = 5;
-        while (startingNumber <= 15) {
-            System.out.println("Current number is: " + startingNumber);
-            startingNumber++;
-
-
-        }
-
-        int addTwos = 0;
-        do {
-            System.out.println(addTwos);
-            addTwos += 2;
-        } while (addTwos <= 100);
-
-        int minusFive = 100;
-        do {
-            System.out.println(minusFive);
-            minusFive -= 5;
-        } while (minusFive >= -10);
-
-        int square = 2;
-        do {
-            System.out.println(square);
-            square *= square;
-        } while (square < 1000000 & (square > 1));
+//        int startingNumber = 5;
+//        while (startingNumber <= 15) {
+//            System.out.println("Current number is: " + startingNumber);
+//            startingNumber++;
+//
+//
+//        }
+//
+//        int addTwos = 0;
+//        do {
+//            System.out.println(addTwos);
+//            addTwos += 2;
+//        } while (addTwos <= 100);
+//
+//        int minusFive = 100;
+//        do {
+//            System.out.println(minusFive);
+//            minusFive -= 5;
+//        } while (minusFive >= -10);
+//
+//        int square = 2;
+//        do {
+//            System.out.println(square);
+//            square *= square;
+//        } while (square < 1000000 & (square > 1));
 
         // Refactoring previous code via for loop
 
@@ -45,6 +45,24 @@ public class ControlFlowExercises {
 //        for (int lessFive = 100; lessFive >= -10; lessFive -= 5) {
 //            System.out.print("\n" + lessFive);
 //        }
+
+        int i = 1;
+        while (i < 100) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            if (i % 3 == 0) {
+                System.out.println(" Fizz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+
 
     }
 }
