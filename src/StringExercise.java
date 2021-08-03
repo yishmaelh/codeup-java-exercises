976aa9884f65aa9f341ba79a6aca3319050b9068
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-03 11:10:55 -0500

    first commit

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+
+    public static void main( String[] args){
+
+        String firstString = "We don't need no education";
+        System.out.println(firstString);
+        String secondString = "We don't need no thought control";
+        System.out.println(secondString);
+        String thirdString = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(thirdString);
+    }
+}
