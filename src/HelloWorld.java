42e5f764553ffb4595a7e4c0b08b43ac983aba28
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-03 11:25:58 -0500

    exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,4 @@
-public class HelloPolaris {
+public class HelloWorld {
 
     public static void main(String[] args) {
 
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,11 +2,17 @@ public class StringExercise {
 
     public static void main( String[] args){
 
-        String firstString = "We don't need no education";
+        String firstString = "We don't need no education" + "\n" + "We don't need no thought control";;
         System.out.println(firstString);
-        String secondString = "We don't need no thought control";
+
+        String secondString = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(secondString);
-        String thirdString = "Check \"this\" out!, \"s inside of \"s!";
+
+        String thirdString = "In windows, the main drive is usually C:\\";
         System.out.println(thirdString);
+
+        String fourthString = " I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(fourthString);
     }
 }
