ca2f79cac51db259978706f8e89ddd7b007ef017
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-04 09:48:08 -0500

    reformatted to allow testing, and printed test problems

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,9 @@ public class MethodsExercises {
 
     public static double add(double x, double y) {
         return x + y;
+
     }
+
 
     public static double subtract(double num1, double num2) {
         return num1 - num2;
@@ -19,5 +21,17 @@ public class MethodsExercises {
     public static double modulus(double x, double y) {
         return x % y;
     }
+
+    public static void main (String[] args) {
+
+        System.out.println(add(6, 7));
+        System.out.println(subtract( 10, 8));
+        System.out.println(multiple(5, 20));
+        System.out.println(divide( 25, 5));
+        System.out.println(modulus(100, 10));
+
+    }
+
+
 
 }
