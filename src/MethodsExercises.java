37540b59ac5ea55dd7713a9f31ebad3e9cf2c6c1
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-04 09:34:44 -0500

    arithmetic

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,6 +1,23 @@
 public class MethodsExercises {
 
-    public static void main(String args){
-
+    public static double add(double x, double y) {
+        return x + y;
     }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiple(double number1, double number2) {
+        return number1 * number2;
+    }
+
+    public static double divide(double h, double i) {
+        return h / i;
+    }
+
+    public static double modulus(double x, double y) {
+        return x % y;
+    }
+
 }
