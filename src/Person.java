f2e868ea860337a11888644a523da2512e05c54b
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-05 15:16:59 -0500

    sayHello

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,17 @@
 public class Person {
 
+    private String name;
+    public String getName(){
+        return name;
+    }
+
+public void setName(String name){
+        this.name = name;
+        System.out.println("The name entered is: " + name);
+}
+
+public void sayHello(){
+        System.out.println("Hello, " + name);
+}
 
 }
