ac6f9d8fb908fc35bef93041a7d5c25c968e4a2a
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-05 16:49:36 -0500

    started testing getter and setter

--- src/Person.java
+++ src/Person.java
@@ -1,27 +1,35 @@
 public class Person {
 
     private String name;
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 
-public void setName(String name){
+    public void setName(String name) {
         this.name = name;
         System.out.println("The name entered is: " + name);
-}
+    }
 
-public void sayHello(){
+    public void sayHello() {
         System.out.println("Hello, " + name);
-}
+    }
 
 //This is a constructor to set the entered name to the string
 
-public Person(String name){
+    public Person(String name) {
         this.name = name;
-}
+    }
 
-public static void main(String[] args){
+    public static void main(String[] args) {
+        Person person = new Person("Lewis");
+        System.out.println(person.getName());
+        person.sayHello();
+        person.setName("Joe");
+        System.out.println(person.getName());
 
-}
+
+
+    }
 
 }
