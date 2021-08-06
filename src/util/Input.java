a384cdac58d64c69b474acdbbd0407a538d95a3a
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-05 19:21:43 -0500

    userInput

---
+++ src/util/Input.java
@@ -0,0 +1,12 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner userInput;
+
+    public Input(){
+        this.userInput = new Scanner(System.in);
+    }
+
+}
