7ceb6729b989f60914ad2b6a9786b12513c704b8
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-06 10:37:43 -0500

    adjustments

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -7,7 +7,7 @@ public class InputTest {
         System.out.println(input.getString());
         System.out.println(input.yesNo());
         System.out.println(input.getInt());
-        System.out.println(input.getDouble());
+        System.out.println(input.getDouble(1,15));
         System.out.println(input.getDouble());
     }
 }
