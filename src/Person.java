aeda098aed17c59b72e66147afcfda12028e0784
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-05 19:06:04 -0500

    test test

--- src/Person.java
+++ src/Person.java
@@ -28,6 +28,32 @@ public class Person {
         person.setName("Joe");
         System.out.println(person.getName());
 
+        // These should return true
+        // EDIT: only one was true; == in java doesn't compare value, but reference
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+        // This should be true
+        // Is true
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        // Should output John and John, then Jane and Jane
+        // Outputs as expected
+//
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+
 
 
     }
