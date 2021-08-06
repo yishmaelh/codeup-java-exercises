46b57737f46ab009eae250d7935902ea4aab9644
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-06 16:38:30 -0500

    square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,4 +5,19 @@ public class Square extends Rectangle {
     public Square(int length, int width) {
         super(length, width);
     }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWidth(int width){
+        this.width = width;
+    }
+
+    public  int getArea(){
+        return width*length;
+    }
+    public int getPerimeter(){
+        return width*4;
+    }
 }
