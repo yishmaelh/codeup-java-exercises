28dc868560df65ac34871876987d4534f4800a55
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-08 16:22:47 -0500

    managed to figure out the bonus as well

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,22 +1,29 @@
 package shapes;
-
-
 import util.Input;
+
+import javax.print.MultiDocPrintService;
 
 public class CircleApp {
 
-    public static void main(String[] args){
+    private static int userMadeCircle = 0;
+    public static void main(String[] args) {
 
+        boolean additionalCircle;
 
         do {
             System.out.println("Enter your circle's radius: ");
             Input inputRadius = new Input();
             double radius = inputRadius.getDouble();
             Circle circle = new Circle(radius);
+            userMadeCircle += 1;
             System.out.println("The area is: " + circle.getArea());
-            System.out.println("The circumference is: " +  circle.getCircumference());
+            System.out.println("The circumference is: " + circle.getCircumference());
+            System.out.println("Would you like to create another circle?");
+            Input inputForCircle = new Input();
+            additionalCircle = inputForCircle.yesNo();
+        } while (additionalCircle);
+        System.out.println("You have created " + userMadeCircle + "circles.");
 
-
-        }
     }
 }
+
