0090042fbd3a64dc2a93d6fb44c03275eaff9364
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-09 15:15:24 -0500

    proctected length and width

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,5 +1,14 @@
 package shapes;
 
-abstract class Quadrilateral extends Shape {
+abstract class Quadrilateral extends Shape implements Measurable{
+
+    protected double length;
+    protected double width;
+
+    public Quadrilateral(){
+        length = this.length;
+        width = this.width;
+    }
+
 
 }
