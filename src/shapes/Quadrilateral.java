55eb27c7fa3082bf971f0be01177b4aef4aecdca
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-09 15:40:24 -0500

    Override

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,6 +1,6 @@
 package shapes;
 
-abstract class Quadrilateral extends Shape implements Measurable{
+public abstract class Quadrilateral extends Shape implements Measurable{
 
     protected double length;
     protected double width;
--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,4 +1,34 @@
-//package shapes;
+package shapes;
+
+public class Rectangle extends Quadrilateral implements Measurable{
+
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double setLength() {
+        return 0;
+    }
+
+    @Override
+    public double setWidth() {
+        return 0;
+    }
+}
+
+
+
+
+
+
 //
 ////public class Rectangle {
 ////    protected int length;
