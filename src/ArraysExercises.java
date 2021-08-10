65409b4c7656a5ea445816939ccdc9c8e76ca386
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-10 11:45:43 -0500

    Person

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static void main(String[] args){
+
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(Arrays.toString(numbers));
+
+        Person[] gundamPilots = new Person[3];
+        gundamPilots[0] = new Person("Judau");
+        gundamPilots[1] = new Person("Domon");
+        gundamPilots[2] = new Person("Allenby");
 
     }
 }
