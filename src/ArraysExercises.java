44befb21a1dfa55659bdeec4238c71c80a48d847
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-10 14:22:34 -0500

    arrays of random words

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int[] numbers = {1,2,3,4,5};
+        int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
 
         Person[] gundamPilots = new Person[3];
         gundamPilots[0] = new Person("Judau");
         gundamPilots[1] = new Person("Domon");
         gundamPilots[2] = new Person("Allenby");
-        for( int i = 0; i < gundamPilots.length; i++){
+        for (int i = 0; i < gundamPilots.length; i++) {
             System.out.println(gundamPilots[i].getName());
         }
 
@@ -19,12 +19,13 @@ public class ArraysExercises {
 
 
     }
-    public static Person[] addPerson(Person[] people, Person Jerid){
+
+    public static Person[] addPerson(Person[] people, Person Jerid) {
         //Use copy to create new array equal to array length+1
         //Add parameter to new array
         //Return new array with added parameter
-        Person[] newPerson = Arrays.copyOf(people, people.length+1);
-        newPerson[newPerson.length -1] = Jerid;
+        Person[] newPerson = Arrays.copyOf(people, people.length + 1);
+        newPerson[newPerson.length - 1] = Jerid;
         return newPerson;
 
     }
---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,14 @@
+public class ServerNameGenerator {
+
+public static void main(String[] args) {
+    String[] nouns = {"Robot", "Bugle", "Stargazer", "Jupiter", "Stranger", "Pizza", "Turbine", "Sword", "Lemon", " Turntable"};
+
+    String[] adjectives = {"Racy", "Burning", "Daring", "Proud", "Misty", "Bashful", "Trusting", "Talented", "Vigorous", "Brave"};
+    System.out.println("Here is your server name: ");
+    System.out.println();
+}
+
+
+
+}
+
