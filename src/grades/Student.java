9f0a8a0efe3cc6e0996ac96176074c4b37e0d3b9
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-11 11:04:15 -0500

    student and grades setter

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,14 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String studentName;
+    private ArrayList<Integer> grades;
+
+    public void setStudentName(String nameOfStudent){
+        this.studentName = nameOfStudent;
+        this.grades =  new ArrayList<>();
+    }
 }
