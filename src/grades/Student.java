a296e7c8fb40526689270e2ed9f4b7d30b355c0e
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-11 11:29:23 -0500

    grade average

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -20,5 +20,17 @@ public class Student {
         return grades;
     }
 
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        double sumOfGrades = 0;
+        double amountOfGrades = grades.size();
+        for(Integer grade :grades){
+            sumOfGrades += grade;
+        }
+        return sumOfGrades/amountOfGrades;
+    }
 
 }
