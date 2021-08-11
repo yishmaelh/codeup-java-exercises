c960af0e16081ac3c9ee8a4ccea7a928523b1c11
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-11 18:08:14 -0500

    hashmap

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,8 +1,13 @@
 package grades;
 
+import java.util.HashMap;
+
+import java.util.HashMap;
+
 public class GradesApplication {
 
     public static void main(String[] args) {
 
+        HashMap<String, String> students = new HashMap<>();
     }
 }
