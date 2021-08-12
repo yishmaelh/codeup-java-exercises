9eac68cbca8133cea4ecb15cee4dcc7fe5a5e585
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-08-11 19:25:52 -0500

    added grades

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,18 +3,33 @@ package grades;
 import java.util.HashMap;
 
 
-
 public class GradesApplication {
 
     public static void main(String[] args) {
         Student student1 = new Student();
-        Student student2 = new Student();
-        Student student3 = new Student();
-        Student student4 = new Student();
         student1.setStudentName("Yish");
+        student1.addGrade(87);
+        student1.addGrade(98);
+        student1.addGrade(96);
+
+        Student student2 = new Student();
         student2.setStudentName("Blaze");
+        student2.addGrade(76);
+        student2.addGrade(65);
+        student2.addGrade(94);
+
+        Student student3 = new Student();
         student3.setStudentName("Viz");
+        student3.addGrade(88);
+        student3.addGrade(76);
+        student3.addGrade(87);
+
+        Student student4 = new Student();
         student4.setStudentName("Lurati");
+        student4.addGrade(58);
+        student4.addGrade(76);
+        student4.addGrade(100);
+
 
     }
 
